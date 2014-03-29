44c66e18aa8bda309a01877b991ad92b04b627c1
Josh Ellithorpe <quest@mac.com>
2014-03-28 17:22:51 -0700

    Fixed broken tests

--- junit-formatter.go
+++ junit-formatter.go
@@ -103,7 +103,7 @@ func JUnitReportXML(report *Report, w io.Writer) error {
 	writer := bufio.NewWriter(w)
 
 	// remove newline from xml.Header, because xml.MarshalIndent starts with a newline
-	writer.WriteString(xml.Header[:len(xml.Header)-1])
+	writer.WriteString(xml.Header[:len(xml.Header)])
 	writer.Write(bytes)
 	writer.WriteByte('\n')
 	writer.Flush()
