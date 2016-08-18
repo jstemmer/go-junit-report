8fbf57721ae6d2443d7e384eb77119c6a7d8c1d6
Ryan Bourgeois <bluedragonx@gmail.com>
2016-08-18 11:39:04 -0700

    Allow output to be written to file and input to go to stdout.

--- go-junit-report.go
+++ go-junit-report.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"io"
 
 	"github.com/jstemmer/go-junit-report/parser"
 )
@@ -12,26 +13,39 @@ var (
 	noXMLHeader bool
 	packageName string
 	setExitCode bool
+	outFile     string
 )
 
 func init() {
 	flag.BoolVar(&noXMLHeader, "no-xml-header", false, "do not print xml header")
 	flag.StringVar(&packageName, "package-name", "", "specify a package name (compiled test have no package name in output)")
 	flag.BoolVar(&setExitCode, "set-exit-code", false, "set exit code to 1 if tests failed")
+	flag.StringVar(&outFile, "out", "", "write output to file instead of stdout")
 }
 
 func main() {
 	flag.Parse()
 
+	var output io.Writer = os.Stdout
+	var input io.Reader = os.Stdin
+	if outFile != "" {
+		var err error
+		if output, err = os.Create(outFile); err != nil {
+			fmt.Printf("Error opening file: %s\n", err)
+			os.Exit(1)
+		}
+		input = io.TeeReader(os.Stdin, os.Stdout)
+	}
+
 	// Read input
-	report, err := parser.Parse(os.Stdin, packageName)
+	report, err := parser.Parse(input, packageName)
 	if err != nil {
 		fmt.Printf("Error reading input: %s\n", err)
 		os.Exit(1)
 	}
 
 	// Write xml
-	err = JUnitReportXML(report, noXMLHeader, os.Stdout)
+	err = JUnitReportXML(report, noXMLHeader, output)
 	if err != nil {
 		fmt.Printf("Error writing XML: %s\n", err)
 		os.Exit(1)
