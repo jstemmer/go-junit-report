e5c84c941a66f62ed9b7ba0c3764c6653c861ae6
ujiro99 <siro.cola@gmail.com>
2018-01-21 19:32:58 +0900

    [add] Enables to build on travis.

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+bin_name=$1
+
+GOOS=linux   GOARCH=386   go build -ldflags="-s -w" -o ./dist/linux_386_${bin_name}
+GOOS=linux   GOARCH=amd64 go build -ldflags="-s -w" -o ./dist/linux_64_${bin_name}
+GOOS=windows GOARCH=386   go build -ldflags="-s -w" -o ./dist/windows_386_${bin_name}.exe
+GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o ./dist/windows_64_${bin_name}.exe
+GOOS=darwin  GOARCH=386   go build -ldflags="-s -w" -o ./dist/darwin_386_${bin_name}
+GOOS=darwin  GOARCH=amd64 go build -ldflags="-s -w" -o ./dist/darwin_64_${bin_name}
+
+cd dist
+
+mv  linux_386_${bin_name}       ${bin_name}
+zip linux_386_${bin_name}       ${bin_name}     -qm
+mv  linux_64_${bin_name}        ${bin_name}
+zip linux_64_${bin_name}        ${bin_name}     -qm
+mv  windows_386_${bin_name}.exe ${bin_name}.exe
+zip windows_386_${bin_name}     ${bin_name}.exe -qm
+mv  windows_64_${bin_name}.exe  ${bin_name}.exe
+zip windows_64_${bin_name}      ${bin_name}.exe -qm
+mv  darwin_386_${bin_name}      ${bin_name}
+zip darwin_386_${bin_name}      ${bin_name}     -qm
+mv  darwin_64_${bin_name}       ${bin_name}
+zip darwin_64_${bin_name}       ${bin_name}     -qm
+
+cd -
