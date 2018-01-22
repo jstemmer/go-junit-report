1c5ff67a2232ee932d53f5b74b210fecac2fe383
ujiro99 <siro.cola@gmail.com>
2018-01-22 21:44:41 +0900

    [update] Add testcase for assertion_macros.cpp

--- junit-report_test.go
+++ junit-report_test.go
@@ -40,6 +40,159 @@ var testCases = []TestCase{
 				},
 			},
 		},
+	}, {
+		name:       "assertion_macros.txt",
+		reportName: "assertion_macros.xml",
+		report: &parser.Report{
+			TestSuites: []*parser.TestSuite{
+				{
+					Name: "assertion_macros.cpp",
+					Time: 4122,
+					Tests: []*parser.Test{
+						{
+							Name:     "normal macros",
+							Time:     993,
+							Result:   parser.FAIL,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "expressions should be evaluated only once",
+							Time:     61,
+							Result:   parser.PASS,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "exceptions-related macros",
+							Time:     224,
+							Result:   parser.FAIL,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "exceptions-related macros for std::exception",
+							Time:     226,
+							Result:   parser.FAIL,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "WARN level of asserts don't fail the test case",
+							Time:     481,
+							Result:   parser.PASS,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "CHECK level of asserts fail the test case but don't abort it",
+							Time:     248,
+							Result:   parser.FAIL,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "REQUIRE level of asserts fail and abort the test case - 1",
+							Time:     115,
+							Result:   parser.FAIL,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "REQUIRE level of asserts fail and abort the test case - 2",
+							Time:     53,
+							Result:   parser.FAIL,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "REQUIRE level of asserts fail and abort the test case - 3",
+							Time:     48,
+							Result:   parser.FAIL,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "REQUIRE level of asserts fail and abort the test case - 4",
+							Time:     50,
+							Result:   parser.FAIL,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "REQUIRE level of asserts fail and abort the test case - 5",
+							Time:     86,
+							Result:   parser.FAIL,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "REQUIRE level of asserts fail and abort the test case - 6",
+							Time:     61,
+							Result:   parser.FAIL,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "REQUIRE level of asserts fail and abort the test case - 7",
+							Time:     50,
+							Result:   parser.FAIL,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "REQUIRE level of asserts fail and abort the test case - 8",
+							Time:     55,
+							Result:   parser.FAIL,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "REQUIRE level of asserts fail and abort the test case - 9",
+							Time:     50,
+							Result:   parser.FAIL,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "REQUIRE level of asserts fail and abort the test case - 10",
+							Time:     50,
+							Result:   parser.FAIL,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "REQUIRE level of asserts fail and abort the test case - 11",
+							Time:     47,
+							Result:   parser.FAIL,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "REQUIRE level of asserts fail and abort the test case - 12",
+							Time:     48,
+							Result:   parser.FAIL,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "all binary assertions",
+							Time:     904,
+							Result:   parser.PASS,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+						{
+							Name:     "some asserts used in a function called by a test case",
+							Time:     272,
+							Result:   parser.PASS,
+							Output:   []string{},
+							Filename: "assertion_macros.cpp",
+						},
+					},
+				},
+			},
+		},
 	},
 }
 
@@ -70,14 +223,17 @@ func TestParser(t *testing.T) {
 			expPkg := expected.TestSuites[i]
 
 			if pkg.Name != expPkg.Name {
+				t.Logf("TestSuite.Name %s", pkg.Name)
 				t.Errorf("TestSuite.Name == %s, want %s", pkg.Name, expPkg.Name)
 			}
 
 			if pkg.Time != expPkg.Time {
+				t.Logf("TestSuite.Name %s", pkg.Name)
 				t.Errorf("TestSuite.Time == %d, want %d", pkg.Time, expPkg.Time)
 			}
 
 			if len(pkg.Tests) != len(expPkg.Tests) {
+				t.Logf("TestSuite.Name %s", pkg.Name)
 				t.Fatalf("TestSuite Tests == %d, want %d", len(pkg.Tests), len(expPkg.Tests))
 			}
 
@@ -85,20 +241,24 @@ func TestParser(t *testing.T) {
 				expTest := expPkg.Tests[j]
 
 				if test.Name != expTest.Name {
+					t.Logf("Test.Name %s", test.Name)
 					t.Errorf("Test.Name == %s, want %s", test.Name, expTest.Name)
 				}
 
 				if test.Time != expTest.Time {
+					t.Logf("Test.Name %s", test.Name)
 					t.Errorf("Test.Time == %d, want %d", test.Time, expTest.Time)
 				}
 
 				if test.Result != expTest.Result {
+					t.Logf("Test.Name %s", test.Name)
 					t.Errorf("Test.Result == %d, want %d", test.Result, expTest.Result)
 				}
 
 				testOutput := strings.Join(test.Output, "\n")
 				expTestOutput := strings.Join(expTest.Output, "\n")
 				if testOutput != expTestOutput {
+					t.Logf("Test.Name %s", test.Name)
 					t.Errorf("Test.Output (%s) ==\n%s\n, want\n%s", test.Name, testOutput, expTestOutput)
 				}
 			}
