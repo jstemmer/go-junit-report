b26a54464ded055a472670c5b24519e16d724304
Matt Delco <delco@google.com>
2018-03-14 16:03:42 -0700

    handle parallel

--- go-junit-report.go
+++ go-junit-report.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/jstemmer/go-junit-report/parser"
+	"github.com/mattdelco/go-junit-report/parser"
 )
 
 var (
--- junit-formatter.go
+++ junit-formatter.go
@@ -8,7 +8,7 @@ import (
 	"runtime"
 	"strings"
 
-	"github.com/jstemmer/go-junit-report/parser"
+	"github.com/mattdelco/go-junit-report/parser"
 )
 
 // JUnitTestSuites is a collection of JUnit test suites.
