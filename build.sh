c48bfb3a849fc0445bc863983bc26918f6b7a104
ujiro99 <siro.cola@gmail.com>
2022-01-30 01:29:48 +0900

    [update] Removed darwin/386 from the build target and added darwin/arm64.

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ GOOS=linux   GOARCH=386   go build -ldflags="-s -w" -o ./dist/linux_386_${bin_na
 GOOS=linux   GOARCH=amd64 go build -ldflags="-s -w" -o ./dist/linux_64_${bin_name}
 GOOS=windows GOARCH=386   go build -ldflags="-s -w" -o ./dist/windows_386_${bin_name}.exe
 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o ./dist/windows_64_${bin_name}.exe
-GOOS=darwin  GOARCH=386   go build -ldflags="-s -w" -o ./dist/darwin_386_${bin_name}
 GOOS=darwin  GOARCH=amd64 go build -ldflags="-s -w" -o ./dist/darwin_64_${bin_name}
+GOOS=darwin  GOARCH=arm64 go build -ldflags="-s -w" -o ./dist/darwin_arm64_${bin_name}
 
 cd dist
 
@@ -19,9 +19,9 @@ mv  windows_386_${bin_name}.exe ${bin_name}.exe
 zip windows_386_${bin_name}     ${bin_name}.exe -qm
 mv  windows_64_${bin_name}.exe  ${bin_name}.exe
 zip windows_64_${bin_name}      ${bin_name}.exe -qm
-mv  darwin_386_${bin_name}      ${bin_name}
-zip darwin_386_${bin_name}      ${bin_name}     -qm
 mv  darwin_64_${bin_name}       ${bin_name}
 zip darwin_64_${bin_name}       ${bin_name}     -qm
+mv  darwin_arm64_${bin_name}    ${bin_name}
+zip darwin_arm64_${bin_name}    ${bin_name}     -qm
 
 cd -
