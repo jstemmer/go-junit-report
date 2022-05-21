fb0aeb9fdaf7793ce8b1ba2f3569ce5b2b385151
Joël Stemmer <stemmertech@gmail.com>
2022-05-21 14:19:38 +0100

    testdata: gofmt testdata/src

--- testdata/src/parallel/pkg_test.go
+++ testdata/src/parallel/pkg_test.go
@@ -8,20 +8,20 @@ import (
 func TestP1(t *testing.T) {
 	t.Parallel()
 	t.Log("t.Log(P1)")
-	time.Sleep(100*time.Millisecond)
+	time.Sleep(100 * time.Millisecond)
 	t.Errorf("P1 error")
 }
 
 func TestP2(t *testing.T) {
 	t.Parallel()
 	t.Log("t.Log(P2)")
-	time.Sleep(50*time.Millisecond)
+	time.Sleep(50 * time.Millisecond)
 	t.Errorf("P2 error")
 }
 
 func TestP3(t *testing.T) {
 	t.Parallel()
 	t.Log("t.Log(P3)")
-	time.Sleep(75*time.Millisecond)
+	time.Sleep(75 * time.Millisecond)
 	t.Errorf("P3 error")
 }
