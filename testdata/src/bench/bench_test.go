5d691c789645cc3c840a8aabd8214e2f30e0e8b1
Joël Stemmer <stemmertech@gmail.com>
2022-05-22 20:52:16 +0100

    testdata: Regenerate benchmark test data and add logging

--- testdata/src/bench/bench_test.go
+++ testdata/src/bench/bench_test.go
@@ -5,7 +5,12 @@ import (
 	"testing"
 )
 
+func TestOne(t *testing.T) {
+	t.Logf("test log")
+}
+
 func BenchmarkOne(b *testing.B) {
+	b.Logf("benchmark log (%d)", b.N)
 	for i := 0; i < b.N; i++ {
 		test(i)
 	}
