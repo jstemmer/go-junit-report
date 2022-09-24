3445b88cb1437df9a4c196a76aa85bf9e82edab1
Joël Stemmer <stemmertech@gmail.com>
2022-09-24 20:52:08 +0100

    parser/gotest: Add fuzzing test for parseLine

--- parser/gotest/gotest_test.go
+++ parser/gotest/gotest_test.go
@@ -221,3 +221,15 @@ func TestParseLine(t *testing.T) {
 		})
 	}
 }
+
+func FuzzParseLine(f *testing.F) {
+	for _, test := range parseLineTests {
+		f.Add(test.input)
+	}
+	f.Fuzz(func(t *testing.T, in string) {
+		events := NewParser().parseLine(in)
+		if len(events) == 0 {
+			t.Fatalf("parseLine(%q) did not return any results", in)
+		}
+	})
+}
